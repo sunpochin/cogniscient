7fc32a27105a3f4e27ff8e2bff02edcd3f5e0383
sunpochin <sunpochin@gmail.com>
2025-08-12 18:36:12 +0800

    feat: adjust position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { Box } from '@mui/material'
 import Image from 'next/image'
 import NavBar from '../components/Navbar'
 // import ClientStoriesCarousel from '../components/client-stories-carousel';
-import AboutUsSection from '../components/AboutCogniscient'
+import AboutCogniscient from '../components/AboutCogniscient'
 import SideDecoration from '../components/side-decoration'
 import WhatIsAIUX from '../components/WhatIsAIUX'
 import WhoAreAIUXFor from '../components/WhoAreAIUXFor'
@@ -40,54 +40,32 @@ const HomePage: NextPage = () => {
         <SideDecoration />
       </div>
       <Image
-        className="absolute top-[0rem] left-[5.611rem] w-[10.556rem] h-[10.556rem] object-cover"
+        className="absolute top-[3rem] left-[1rem] w-[10.556rem] h-[10.556rem] object-cover"
         width={190}
         height={190}
         sizes="100vw"
         alt=""
         src="/cogniscient.png"
       />
-      <div className="mt-20 ml-3 mr-13 relative z-10">
-        <AboutUsSection />
+      <div className="mt-40 ml-20 mr-14 relative z-10">
+        <AboutCogniscient />
       </div>
-      <div className="mt-20 w-3/4 ml-auto mr-12 relative z-10">
+      <div className="mt-20 w-3/4 ml-auto mr-14 relative z-10">
         <WhatIsAIUX />
       </div>
-      <div className="mt-20 ml-3 mr-13 relative z-10">
+      <div className="mt-20 ml-20 mr-14 relative z-10">
         <WhoAreAIUXFor />
       </div>
-      <div className="mt-20 w-3/4 ml-auto mr-12 relative z-10">
+      <div className="mt-20 w-3/4 ml-auto mr-14 relative z-10">
         <StartYourJourney />
       </div>
-      <div className="mt-20 ml-3 mr-13 relative z-10">
+      <div className="mt-20 ml-20 mr-14 relative z-10">
         <OurServices />
       </div>
-      <div className="mt-20 w-3/4 ml-auto mr-12 relative z-10">
+      <div className="mt-20 w-3/4 ml-auto mr-14 relative z-10">
         <WhoAreWe />
       </div>
       <ContactForm />
-
-      {/* <Box className="bg-[#dcebff]" />
-      <div className="absolute top-[293.444rem] left-[21.339rem] text-[#5d85b9] inline-block w-[6.044rem] h-[1.261rem]">
-        Name
-      </div>
-      <Box className="absolute top-[297.006rem] left-[20.411rem] bg-[#dcebff] w-[49.45rem] h-[3.144rem]" />
-      <Box className="absolute top-[301.517rem] left-[20.411rem] bg-[#dcebff] w-[49.45rem] h-[20.083rem]" />
-      <div className="absolute top-[297.95rem] left-[21.339rem] text-[#5d85b9] inline-block w-[6.044rem] h-[1.339rem]">
-        Email
-      </div>
-      <div className="absolute top-[302.461rem] left-[21.339rem] text-[#5d85b9] inline-block w-[10.344rem] h-[1.261rem]">
-        The message
-      </div>
-      <div className="absolute top-[322.306rem] left-[51.467rem] text-[0.667rem] text-[#5d85b9] inline-block w-[21.039rem] h-[0.789rem]">
-        You will receive a copy of the message after you submit
-      </div>
-      <div className="absolute top-[309.95rem] left-[33.25rem] text-[1.333rem] font-semibold">
-        We look forward to hear from you!
-      </div>
-      <div className="absolute top-[285.933rem] left-[20.411rem] text-[3.556rem] font-semibold">
-        Contact Us
-      </div> */}
     </Box>
   )
 }
